5de83e486dc0bcd96ec549344d95a53607652774
Shreya12365 <shreyasshree2000@gmail.com>
2024-10-06 11:37:33 +0530

    var

--- var.sh
+++ var.sh
@@ -1,6 +1,6 @@
 #!bin/bash
-person1=ram
-person2=ragu
+person1=$1
+person2=$2
 echo "$person1: Hi $person2, Good morning"
 echo "$person2: hello, GM, How are you"
 echo "$person1: good, what about you"
